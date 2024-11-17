3238eb38ae1d63e42e32d41604145f42b895f017
Baran Özcan <baranozcan033@gmail.com>
2024-11-17 20:20:19 +0300

    Submit reviews, Approve reviews done

--- frontend/src/components/index.js
+++ frontend/src/components/index.js
@@ -5,6 +5,7 @@ import TopNavBar  from "./common/NavBar/TopNavBar";
 import SubscriptionBanner from "./common/SubscriptionBanner";
 import Footer from "./common/Footer";
 import MainContent from "./mainPage/MainContent";
+import ReviewWindow from "./ReviewWindow";
 
 export {
     LoginForm,
@@ -13,5 +14,6 @@ export {
     TopNavBar,
     SubscriptionBanner,
     Footer,
-    MainContent
+    MainContent,
+    ReviewWindow
 };
